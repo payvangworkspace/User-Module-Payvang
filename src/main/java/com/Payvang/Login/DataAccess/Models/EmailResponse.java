2e393a73b2891521b4e19ae9b4c6caeb9a65dab7
Amit Kumar <amitak3107@gmail.com>
2025-02-15 17:05:38 +0530

    update response format

--- src/main/java/com/Payvang/Login/DataAccess/Models/EmailResponse.java
+++ src/main/java/com/Payvang/Login/DataAccess/Models/EmailResponse.java
@@ -4,14 +4,19 @@ import org.springframework.http.HttpStatus;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
 public class EmailResponse {
 	private String message;
 	private HttpStatus status;
 	
-
+	 public EmailResponse(String message, HttpStatus status) {
+	        this.message = message;
+	        this.status = status;
+	    }
 }
